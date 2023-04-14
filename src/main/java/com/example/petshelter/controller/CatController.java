30a17450d85ec78e236ef24beaaf06a45d3710ab
MarselMingaripov <bard1812@gmail.com>
2023-04-14 13:34:53 +0300

    поправил кошачий контроллер

--- src/main/java/com/example/petshelter/controller/CatController.java
+++ src/main/java/com/example/petshelter/controller/CatController.java
@@ -22,7 +22,6 @@ public class CatController {
     @PostMapping
     @Operation(summary = "Сохранение котов в БД")
     @ApiResponse(responseCode = "200", description = "Запрос выполнен, кот добавлен в БД")
-    @ApiResponse(responseCode = "400", description = "Параметры запроса отсутствуют или имеют некорректный формат")
     @ApiResponse(responseCode = "405", description = "Ошибка валидации")
     @ApiResponse(responseCode = "500", description = "Произошла ошибка, не зависящая от вызывающей стороны")
     public ResponseEntity<Cat> createCat(@RequestBody Cat cat) {
@@ -32,7 +31,7 @@ public class CatController {
     @GetMapping("/{id}")
     @Operation(summary = "Получение данных кота по id")
     @ApiResponse(responseCode = "200", description = " Запрос выполнен, данные получены")
-    @ApiResponse(responseCode = "400", description = "Параметры запроса отсутствуют или имеют некорректный формат")
+    @ApiResponse(responseCode = "404", description = "Не найден по ид")
     @ApiResponse(responseCode = "500", description = "Произошла ошибка, не зависящая от вызывающей стороны")
     public ResponseEntity<Cat> findById(@PathVariable Long id) {
         return ResponseEntity.ok().body(catService.findById(id));
@@ -41,7 +40,7 @@ public class CatController {
     @GetMapping("/find-by-name/{name}")
     @Operation(summary = "Получение данных кота по имени")
     @ApiResponse(responseCode = "200", description = " Запрос выполнен, данные получены")
-    @ApiResponse(responseCode = "400", description = "Параметры запроса отсутствуют или имеют некорректный формат")
+    @ApiResponse(responseCode = "404", description = "Параметры запроса отсутствуют или имеют некорректный формат")
     @ApiResponse(responseCode = "500", description = "Произошла ошибка, не зависящая от вызывающей стороны")
     public ResponseEntity<Cat> findByName(@PathVariable String name) {
         return ResponseEntity.ok().body(catService.findByName(name));
@@ -49,6 +48,7 @@ public class CatController {
 
     @GetMapping
     @Operation(summary = "Получение списка котов находящихся в приюте")
+    @ApiResponse(responseCode = "200", description = " Запрос выполнен, данные получены")
     public ResponseEntity<List<Cat>> findAll() {
         return ResponseEntity.ok().body(catService.findAll());
     }
@@ -57,6 +57,7 @@ public class CatController {
     @Operation(summary = "Изменение данных кота по id")
     @ApiResponse(responseCode = "200", description = "Запрос выполнен, данные изменены")
     @ApiResponse(responseCode = "404", description = "Кота с таким id нет в БД или параметры запроса имеют некорректный формат")
+    @ApiResponse(responseCode = "405", description = "Ошибка валидации")
     @ApiResponse(responseCode = "500", description = "Произошла ошибка, не зависящая от вызывающей стороны")
     public ResponseEntity<Cat> updateById(@PathVariable Long id, @RequestBody Cat cat) {
         return ResponseEntity.ok().body(catService.updateById(id, cat));
@@ -65,7 +66,7 @@ public class CatController {
     @DeleteMapping("/{id}")
     @Operation(summary = "Удаление данных кота из БД приюта")
     @ApiResponse(responseCode = "200", description = "Запрос выполнен, данные удалены")
-    @ApiResponse(responseCode = "400", description = "Параметры запроса отсутствуют или имеют некорректный формат")
+    @ApiResponse(responseCode = "404", description = "Кота с таким id нет в БД или параметры запроса имеют некорректный формат")
     @ApiResponse(responseCode = "500", description = "Произошла ошибка, не зависящая от вызывающей стороны")
     public ResponseEntity<Cat> deleteById(@PathVariable Long id) {
         return ResponseEntity.ok().body(catService.deleteById(id));
