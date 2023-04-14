66b4084ef66a32a4feb26955ed38f78cfec2b2d7
MarselMingaripov <bard1812@gmail.com>
2023-04-14 12:57:09 +0300

    добавил обработчик ошибок, переделал миграции

---
+++ src/main/java/com/example/petshelter/exception/handler/ExceptionsHandler.java
@@ -0,0 +1,24 @@
+package com.example.petshelter.exception.handler;
+
+import com.example.petshelter.exception.NotFoundInBdException;
+import com.example.petshelter.exception.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionsHandler {
+
+    //404
+    @ExceptionHandler
+    public ResponseEntity<String> notFoundInBdExceptionHandler(NotFoundInBdException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    //405
+    @ExceptionHandler
+    public ResponseEntity<String> validationExceptionHandler(ValidationException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.METHOD_NOT_ALLOWED);
+    }
+}
