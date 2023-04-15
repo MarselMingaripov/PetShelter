e33da37e13f0a500fade6b88b33f6d4ecd2cf417
SergeyGolybtsov <kirsa200@gmail.com>
2023-04-15 18:06:48 +0300

    Доработал тесты.

--- src/test/java/com/example/petshelter/service/CatServiceImplTest.java
+++ src/test/java/com/example/petshelter/service/CatServiceImplTest.java
@@ -1,23 +1,45 @@
 package com.example.petshelter.service;
 
+import com.example.petshelter.entity.Cat;
+import com.example.petshelter.entity.StatusAnimal;
 import com.example.petshelter.repository.CatRepository;
 import com.example.petshelter.service.impl.CatServiceImpl;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.when;
+import java.util.Optional;
+import java.util.function.BooleanSupplier;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CatServiceImplTest {
-    @Mock
-    private CatRepository catRepositoryMock;
-    @InjectMocks
-    private CatServiceImpl outCatMock;
-    @Test
-    public void doTestCat() {
+    private static final Cat VALID_CAT = new Cat("Барсик", 12, true, true, StatusAnimal.HAS_HOUSE);
+    private static final Cat INVALID_CAT = new Cat("Жук", 0, true, true, StatusAnimal.IN_THE_SHELTER);
 
+    @Mock
+    CatRepository catRepositoryMock;
+    @InjectMocks
+    CatServiceImpl catServiceOut;
+
+    @Test
+    @DisplayName("Возвращает true если пользователь с именем есть в списке")
+    void shouldReturnTrue() {
+        when(catRepositoryMock.findByName(VALID_CAT.getName())).thenReturn(Optional.of(VALID_CAT));
+        assertTrue((BooleanSupplier) catServiceOut.findByName(String.valueOf(VALID_CAT)));
+        verify(catRepositoryMock, times(1)).findByName(VALID_CAT.getName());
+    }
+
+    @Test
+    @DisplayName("Возвращает false если пользователя с именем нет в списке")
+    void shouldReturnFalse() {
+        when(catRepositoryMock.findByName(INVALID_CAT.getName())).thenReturn(Optional.of(null));
+        assertTrue((BooleanSupplier) catServiceOut.findByName(String.valueOf(INVALID_CAT)));
+        verify(catRepositoryMock, times(1)).findByName(INVALID_CAT.getName());
     }
 }
--- src/test/java/com/example/petshelter/service/DogServiceImplTest.java
+++ src/test/java/com/example/petshelter/service/DogServiceImplTest.java
@@ -1,22 +1,49 @@
 package com.example.petshelter.service;
 
+import com.example.petshelter.entity.Cat;
+import com.example.petshelter.entity.Dog;
+import com.example.petshelter.entity.StatusAnimal;
+
 import com.example.petshelter.repository.DogRepository;
 import com.example.petshelter.service.impl.DogServiceImpl;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
+
+import java.util.Optional;
+import java.util.function.BooleanSupplier;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 public class DogServiceImplTest {
+    private static final Dog VALID_DOG = new Dog(1L,"Шарик", 12, true, true, StatusAnimal.HAS_HOUSE);
+    private static final Dog INVALID_DOG = new Dog(1L,"Жук", 0, true, true, StatusAnimal.IN_THE_SHELTER);
+
     @Mock
-    private DogRepository dogRepositoryMock;
+    DogRepository dogRepositoryMock;
     @InjectMocks
-    private DogServiceImpl outDogMock;
+    DogServiceImpl dogServiceOut;
 
     @Test
-    public void doTestDog() {
+    @DisplayName("Возвращает true если пользователь с именем есть в списке")
+    void shouldReturnTrue() {
+        when(dogRepositoryMock.findByName(VALID_DOG.getName())).thenReturn(Optional.ofNullable(VALID_DOG));
+        assertTrue((BooleanSupplier) dogServiceOut.findByName(String.valueOf(VALID_DOG)));
+        verify(dogRepositoryMock, times(1)).findByName(VALID_DOG.getName());
+    }
 
+    @Test
+    @DisplayName("Возвращает false если пользователя с именем нет в списке")
+    void shouldReturnFalse() {
+        when(dogRepositoryMock.findByName(INVALID_DOG.getName())).thenReturn(Optional.ofNullable(null));
+        assertTrue((BooleanSupplier) dogServiceOut.findByName(String.valueOf(INVALID_DOG)));
+        verify(dogRepositoryMock, times(1)).findByName(INVALID_DOG.getName());
     }
 }
